d806d520d1a38f6e2e6acb819f05d850e727ca44
Nurzhan Zhussupov <nur76n@mail.ru>
2025-12-09 19:40:51 +0500

    vault backup: 2025-12-09 19:40:51

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Docs",
+    pageTitle: "📖 Docs",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
